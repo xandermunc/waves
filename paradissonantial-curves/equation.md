1e5bab3a91c35987eb0ddd7b80f092df6ba44f5a
xandermunc <xandermunc@gmail.com>
2025-02-12 00:20:28 -0500

    Update equation / add test.html / add inspo and examples

--- paradissonantial-curves/equation.md
+++ paradissonantial-curves/equation.md
@@ -1,5 +1,5 @@
 $$
-\left( \sum_{n=1}^{N} A \sin(f_n \theta+\phi), B \cos(f_v \theta) \right) \theta \in [0, λπ]
+\left( \sum_{n=1}^{N} A \sin(f_n \theta+\phi), B \cos(f_v \theta) \right) \theta \in [0, λ]
 $$
 $$
 B = A \cdot N
@@ -9,30 +9,24 @@ f_n = \frac{a_n}{b_n}
 $$
 
 $$
-f_v = \frac{b^*-1}{b^*}
+f_{v_{standard}} = \frac{b^*-1}{b^*}
 $$
-
-<!-- $$
-f_v = |\frac{P_b-\left(S_P\right)}{P_b} |
-$$
-
-$$
-S_P= \sum_{n=1}^{N} \frac{P_b}{b_n}
-$$
-
-$$
-P_b = \prod_{n=1}^{N} b_n
-$$ -->
 
 test the wavelength against non-fifth-having chords. see if product does something more accurate. 
 
 $$
-λ = 2\left(\text{LCM}\{ b_n \}\right)
+L_b = \text{LCM}\{ b_n \} \\[10pt]
+P_b = \prod_{n=1}^{N} b_n \\[10pt]
+S_P= \sum_{n=1}^{N} \frac{P_b}{b_n} \\
 $$
 
 $$
-M_b = \max_{n=1}^{N} b_n
+λ = 2L_bπ
 $$
+
+<!-- $$
+M_b = \max_{n=1}^{N} b_n
+$$ -->
 
 <!-- $$ 
 b_{n^*} \quad \text{where } n^* = \arg\max_{n=1}^{N} f_n
@@ -42,7 +36,22 @@ $$
 n^* = \arg\min_{n=1}^{N} \{ a_n, b_n \}
 $$ -->
 
-<!-- Full Equation
+Full Equation
 $$
 \left( \sum_{n=1}^{N} A \sin(\frac{a_n}{b_n} \theta+\phi), A \cdot N \cos(\frac{\prod_{n=1}^{N} b_n-\left(N-1\right)}{\prod_{n=1}^{N} b_n}  \theta) \right) 
+$$
+
+<!-- $$
+L_b \neq P_b \Rightarrow \\
+\forall n, \quad \neg (L_b = P_b) \Rightarrow 
 $$ -->
+
+$$
+L_b \neq P_b \Rightarrow \\[10pt]
+f_v = |\frac{P_b-S_P}{P_b}| \\[10pt]
+$$
+
+$$
+L_b = P_b \Rightarrow \\[10pt]
+f_v = |\frac{P_b-S_P-1}{P_b}| \\[10pt]
+$$
--- paradissonantial-curves/pd.r
+++ paradissonantial-curves/pd.r
@@ -1,8 +1,9 @@
 pdf("1-10-6.pdf") 
 t <- seq(0, 2 * pi, length.out = 1000)
 phi <- 0
-x <- sin(t + phi) + sin(10 * t + phi)
-y <- 2 * cos(6 * t)
+x <- sin(t + phi) + sin(A * t + phi)
+y <- 2 * cos(t)
+A <- 1
 plot(x, y,
   type = "l", bty = "n", col = "#09f", lwd = 2, xlab = "x", ylab = "y",
   main = "1:10:6", xlim = c(-2, 2), ylim = c(-2, 2), asp = 1
@@ -26,7 +27,7 @@ for (i in seq_along(phi_values)) {
   frames[[i]] <- image_read(paste0("frame_", i, ".png"))
 }
 animation <- image_animate(image_join(frames), fps = 25)
-image_write(animation, "phi_animation_high_quality.gif")
+image_write(animation, "phi_animation.gif")
 file.remove(paste0("frame_", seq_along(phi_values), ".png"))
 
 install.packages("rgl", type = "source")
@@ -54,3 +55,155 @@ open3d()
 tube <- cylinder3d(cbind(x, y, z), radius = 0.05, sides = 100)
 shade3d(tube, col = "blue")
 writeOBJ("curve.obj")
+
+
+
+
+library(magick)
+A_values <- seq(3/2, 1, length.out = 20)
+t <- seq(0, 2 * pi, length.out = 1000)
+phi <- 0
+images <- list()
+for (A in A_values) {
+  x <- sin(t + phi) + sin(A * t + phi)
+  y <- 2 * cos(t)
+    png_filename <- tempfile(fileext = ".png")
+  png(png_filename)
+  plot(x, y,
+       type = "l", bty = "n", col = "#09f", lwd = 2,
+       xlab = "x", ylab = "y", main = "1:10:6",
+       xlim = c(-2, 2), ylim = c(-2, 2), asp = 1)
+  dev.off()
+    images[[length(images) + 1]] <- image_read(png_filename)
+}
+animation <- image_animate(image_join(images), fps = 10)
+image_write(animation, "1-10-6.gif")
+
+
+library(magick)
+A_values <- seq(1, 3/2, length.out = 20)
+start_length <- 2 * pi
+end_length <- 4 * pi
+phi <- 0
+images <- list()
+for (i in 1:length(A_values)) {
+  current_length <- seq(start_length, end_length, length.out = length(A_values))[i]
+  t_values <- seq(0, current_length, length.out = 1000)
+  x <- sin(t_values + phi) + sin(A_values[i] * t_values + phi)
+  y <- 2 * cos(t_values)
+  png_filename <- tempfile(fileext = ".pdf")
+  png(png_filename)
+  plot(x, y,
+       type = "l", bty = "n", col = "#09f", lwd = 2,
+       xlab = "x", ylab = "y", main = "1:3/2",
+       xlim = c(-2, 2), ylim = c(-2, 2), asp = 1)
+  dev.off()
+  images[[length(images) + 1]] <- image_read(png_filename)
+}
+animation <- image_animate(image_join(images), fps = 10)
+image_write(animation, "1.gif")
+
+n_frames <- 100
+pdf("1-10-6.pdf")
+for (i in seq(1, 3/2, length.out = n_frames)) {
+  A <- i
+  t <- seq(0, 2 * pi, length.out = 1000)
+  phi <- 0
+  x <- sin(t + phi) + sin(A * t + phi)
+  y <- 2 * cos(t)
+  plot(x, y,
+       type = "l", bty = "n", col = "#09f", lwd = 2, xlab = "x", ylab = "y",
+       main = paste("A =", round(A, 2)), 
+       xlim = c(-2, 2), ylim = c(-2, 2), asp = 1
+  )
+  Sys.sleep(0.1) 
+}
+dev.off()
+
+# n_frames <- 100
+# start_length <- 2 * pi
+# end_length <- 4 * pi
+# pdf("1-10-6.pdf")
+# for (i in seq(1, 3/2, length.out = n_frames)) {
+#   f1 <- 1
+#   f2 <- i
+#   current_length <- seq(start_length, end_length, length.out = n_frames)[which(seq(1, 3/2, length.out = n_frames) == i)]
+#   t <- seq(0, current_length, length.out = 1000)
+#   phi <- 0
+#   x <- sin(f1 * t + phi) + sin(f2 * t + phi)
+#   y <- 2 * cos(t)
+#   plot(x, y,
+#     type = "l", bty = "n", col = "#09f", lwd = 2, xlab = "", ylab = "",
+#     xlim = c(-2, 2), ylim = c(-2, 2), asp = 1,
+#     xaxt = "n", yaxt = "n", axes = FALSE
+#   )
+#   Sys.sleep(0.1)
+# }
+# dev.off()
+
+n_frames <- 100
+start_length <- 20 * pi
+end_length <- 16 * pi
+f1_values <- seq(1, 1, length.out = n_frames)
+f2_values <- seq(6/5, 5/4, length.out = n_frames)
+f3_values <- seq(3/2, 3/2, length.out = n_frames)
+f4_values <- seq(9/5, 15/8, length.out = n_frames)
+fv_values <- seq(4/5, 3/4, length.out = n_frames)
+A <- 4
+pdf("1-10-6.pdf")
+for (i in seq_len(n_frames)) {
+  f1 <- f1_values[i]
+  f2 <- f2_values[i]
+  f3 <- f3_values[i]
+  f4 <- f4_values[i]
+  fv <- fv_values[i]
+  current_length <- seq(start_length, end_length, length.out = n_frames)[i]
+  t <- seq(0, current_length, length.out = 1000)
+  phi <- 0
+  x <- sin(f1 * t + phi) + sin(f2 * t + phi) + sin(f3 * t + phi) + sin(f4 * t + phi)
+  y <- A * cos(fv * t)
+  plot(x, y,
+       type = "l", bty = "n", col = "#09f", lwd = 2, xlab = "", ylab = "",
+       xlim = c(-A, A), ylim = c(-A, A), asp = 1,
+       xaxt = "n", yaxt = "n", axes = FALSE
+  )
+  Sys.sleep(0.1)
+}
+dev.off()
+
+
+library(magick)
+n_frames <- 100
+start_length <- 16 * pi
+end_length <- 2 * pi
+f1_values <- seq(1, 1, length.out = n_frames)
+f2_values <- seq(5/4, 4/4, length.out = n_frames)
+f3_values <- seq(3/2, 2/2, length.out = n_frames)
+f4_values <- seq(15/8, 8/8, length.out = n_frames)
+fv_values <- seq(3/4, 4/4, length.out = n_frames)
+A_values <- seq(4, 4, length.out = n_frames)
+B_values <- seq(1, 1, length.out = n_frames)
+pdf("test.pdf")
+for (i in seq_len(n_frames)) {
+  f1 <- f1_values[i]
+  f2 <- f2_values[i]
+  f3 <- f3_values[i]
+  f4 <- f4_values[i]
+  fv <- fv_values[i]
+  A <- A_values[i]
+  B <- B_values[i]
+  current_length <- seq(start_length, end_length, length.out = n_frames)[i]
+  t <- seq(0, current_length, length.out = 1000)
+  phi <- 0
+  x <- B * sin(f1 * t + phi) + B * sin(f2 * t + phi) + B * sin(f3 * t + phi) + B * sin(f4 * t + phi)
+  y <- A * cos(fv * t)
+  plot(x, y,
+       type = "l", bty = "n", col = "#09f", lwd = 2, xlab = "", ylab = "",
+       xlim = c(-4, 4), ylim = c(-4, 4), asp = 1,
+       xaxt = "n", yaxt = "n", axes = FALSE
+  )
+}
+dev.off()
+pdf_images <- image_read_pdf("test.pdf")
+animation <- image_animate(pdf_images, fps = 50)
+image_write(animation, "test.gif")
