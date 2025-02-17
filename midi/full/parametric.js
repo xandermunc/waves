048c2e9f2e0b885d2fadb389c49ae1d0dc436fb4
xandermunc <xandermunc@gmail.com>
2025-02-17 14:33:48 -0500

    Add keyboard js

---
+++ midi/full/parametric.js
@@ -0,0 +1,155 @@
+const activeNotes = new Set();
+let phi = 0;
+let velocity = 0.01;
+document.getElementById("velocitySlider").addEventListener("input", event => {
+    velocity = parseFloat(event.target.value);
+});
+
+function drawParametricCurve(svg, funcX, funcY, tMin, tMax, steps, scaleX, scaleY, translateX, translateY) {
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    let d = "";
+    if (activeNotes.size === 0) return;
+    for (let i = 0; i <= steps; i++) { // this is what draws the points and connects them with straight lines. maybe updating this will fix the detail issue
+        const t = tMin + (tMax - tMin) * (i / steps);
+        const x = funcX(t) * scaleX + translateX;
+        const y = funcY(t) * scaleY * -1 + translateY;
+        if (i === 0) {
+            d += `M ${x} ${y}`; // move to 
+        } else {
+            d += ` L ${x} ${y}`; // line to 
+        }
+    }
+
+    path.setAttribute("d", d);
+    path.setAttribute("stroke", "#e1e1e1");
+    path.setAttribute("stroke-width", "2");
+    path.setAttribute("fill", "none");
+    svg.appendChild(path);
+}
+
+function getJustIntonationInterval(note) {
+    const justIntonationRatios = {
+        'C': 1, 'C#': 16 / 15, 'D': 9 / 8, 'D#': 6 / 5, 'E': 5 / 4,
+        'F': 4 / 3, 'F#': 45 / 32, 'G': 3 / 2, 'G#': 8 / 5, 'A': 5 / 3,
+        'A#': 9 / 5, 'B': 15 / 8
+    };
+    return justIntonationRatios[note] || 1;
+}
+
+function getBIntervals(note) {
+    const bIntervals = {
+        'C': 1, 'C#': 15, 'D': 8, 'D#': 5, 'E': 4,
+        'F': 3, 'F#': 32, 'G': 2, 'G#': 5, 'A': 3,
+        'A#': 5, 'B': 8
+    };
+    return bIntervals[note] || 1;
+}
+
+// function getJustIntonationInterval(note) {
+//     const justIntonationRatios = {
+//         'F': 1, 'F#': 16 / 15, 'G': 9 / 8, 'G#': 6 / 5, 'A': 5 / 4,
+//         'A#': 4 / 3, 'B': 45 / 32, 'C': 3 / 2, 'C#': 8 / 5,
+//         'D': 5 / 3, 'D#': 9 / 5, 'E': 15 / 8
+//     };
+//     return justIntonationRatios[note] || 1;
+// }
+
+// function getBIntervals(note) {
+//     const bIntervals = {
+//         'F': 1, 'F#': 15, 'G': 8, 'G#': 5, 'A': 4,
+//         'A#': 3, 'B': 32, 'C': 2, 'C#': 5,
+//         'D': 3, 'D#': 5, 'E': 8
+//     };
+//     return bIntervals[note] || 1;
+// }
+
+function getNoteFromMIDINumber(noteNumber) {
+    const notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+    return notes[noteNumber % 12];
+}
+
+function getOctaveFromMIDINumber(noteNumber) {
+    return Math.floor(noteNumber / 12) - 1;
+}
+
+function gcd(a, b) {
+    return b === 0 ? a : gcd(b, a % b);
+}
+
+function lcm(a, b) {
+    return (a * b) / gcd(a, b);
+}
+
+function lcmOfArray(arr) {
+    return arr.reduce((acc, val) => lcm(acc, val), 1);
+}
+
+function onMIDIMessage(event) {
+    const command = event.data[0];
+    const noteNumber = event.data[1];
+    const velocity = event.data[2];
+
+    if (command >= 144 && command <= 159 && velocity > 0) {
+        activeNotes.add(noteNumber);
+    } else if ((command >= 128 && command <= 143) || (command >= 144 && command <= 159 && velocity === 0)) {
+        activeNotes.delete(noteNumber);
+    }
+}
+
+function animate() {
+    phi += velocity;
+    if (phi > 2 * Math.PI) {
+        phi -= 2 * Math.PI;
+    }
+
+    const svgCanvas = document.getElementById("svgCanvas");
+    svgCanvas.innerHTML = '';
+
+    const intervals = Array.from(activeNotes).map(noteNumber => {
+        const note = getNoteFromMIDINumber(noteNumber);
+        const interval = getJustIntonationInterval(note);
+        const octave = getOctaveFromMIDINumber(noteNumber);
+        if (octave > 4) {
+            return interval * Math.pow(2, octave - 4);
+        } else if (octave < 4) {
+            return interval / Math.pow(2, 4 - octave);
+        } else {
+            return interval;
+        }
+    });
+
+    const bIntervals = Array.from(activeNotes).map(noteNumber => {
+        const note = getNoteFromMIDINumber(noteNumber);
+        const octave = getOctaveFromMIDINumber(noteNumber);
+        let bInterval = getBIntervals(note);
+        if (octave > 4) {
+            bInterval *= Math.pow(2, octave - 4);
+        } else if (octave < 4) {
+            bInterval *= Math.pow(2, 4 - octave);
+        }
+        return bInterval;
+    });
+    const bInterval = bIntervals.length > 0 ? lcmOfArray(bIntervals) : 1;
+    const B = activeNotes.size;
+
+    drawParametricCurve(svgCanvas,
+        t => intervals.reduce((sum, interval) => sum + Math.sin(interval * t + phi), 0), // x
+        // t => intervals.reduce((sum, interval) => sum + Math.cos(interval * t + phi), 0), // comment out y (the line below) and uncomment this to see top view
+        t => B * Math.cos(t), // y
+        0, 2 * bInterval * Math.PI, 1000, // bInterval is the LCM of the denominators of each JI interval 
+        200 / B, 200 / B, 250, 250); // removing B changes the scale based on the number of notes (this didn't work on zoom for some reason)
+
+    requestAnimationFrame(animate);
+}
+
+if (navigator.requestMIDIAccess) {
+    navigator.requestMIDIAccess().then(midiAccess => {
+        midiAccess.inputs.forEach(input => {
+            input.onmidimessage = onMIDIMessage;
+        });
+    });
+} else {
+    console.error("MIDI not supported in this browser.");
+}
+
+requestAnimationFrame(animate);
