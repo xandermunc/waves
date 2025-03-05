f1633089cfa7c81de03599a7bc93437d3518aa3c
xandermunc <xandermunc@gmail.com>
2025-03-05 14:12:27 -0500

    Update intro page

--- our-place-in-waves/js/AFVW/amplitude.js
+++ our-place-in-waves/js/AFVW/amplitude.js
@@ -1,68 +1,104 @@
-const velocitySlider = document.getElementById('velocity');
-const frequencySlider = document.getElementById('frequency');
-const wavelengthSlider = document.getElementById('wavelength');
+const velocitySlider = document.getElementById('solo-velocity');
+const frequencySlider = document.getElementById('solo-frequency');
+const wavelengthSlider = document.getElementById('solo-wavelength');
+
+const velocityValueDisplay = document.getElementById('velocityValue');
+const frequencyValueDisplay = document.getElementById('frequencyValue');
+const wavelengthValueDisplay = document.getElementById('wavelengthValue');
+
 const colorRect = document.getElementById('color-rect');
 const initialWavelength = 160; // #09f
 colorRect.style.backgroundColor = wavelengthToColor(initialWavelength);
+
 let velocity = parseFloat(velocitySlider.value);
+
+// Update displays for initial values
+// velocityValueDisplay.textContent = velocity.toFixed(1);
+// frequencyValueDisplay.textContent = frequencySlider.value;
+// wavelengthValueDisplay.textContent = (velocity / frequencySlider.value).toFixed(1);
 
 // Update frequency and wavelength based on velocity
 velocitySlider.addEventListener('input', () => {
     velocity = parseFloat(velocitySlider.value);
     const frequency = parseFloat(frequencySlider.value);
     const wavelength = velocity / frequency;
+
     wavelengthSlider.value = wavelength.toFixed(wavelengthSlider.min);
+    // wavelengthValueDisplay.textContent = wavelength.toFixed(1);
+    // velocityValueDisplay.textContent = velocity.toFixed(1);
 });
 
 // Update wavelength based on frequency
 frequencySlider.addEventListener('input', () => {
     const frequency = parseFloat(frequencySlider.value);
     const wavelength = velocity / frequency;
+
     wavelengthSlider.value = wavelength.toFixed(wavelengthSlider.min);
+    // wavelengthValueDisplay.textContent = wavelength.toFixed(1);
+    // frequencyValueDisplay.textContent = frequency.toFixed(1);
 });
 
 // Update frequency based on wavelength
 wavelengthSlider.addEventListener('input', () => {
     const wavelength = parseFloat(wavelengthSlider.value);
     const frequency = velocity / wavelength;
+
     frequencySlider.value = frequency.toFixed(frequencySlider.min);
+    // frequencyValueDisplay.textContent = frequency.toFixed(1);
+    // wavelengthValueDisplay.textContent = wavelength.toFixed(1);
 });
 
 frequencySlider.addEventListener('input', function () {
     if (parseFloat(velocitySlider.value) <= parseFloat(frequencySlider.value)) {
         velocitySlider.value = frequencySlider.value;
+        // velocityValueDisplay.textContent = frequency.toFixed(1);
+        // wavelengthValueDisplay.textContent = wavelength.toFixed(1);
         wavelengthSlider.value = wavelengthSlider.min;
     } else if (parseFloat(frequencySlider.value) >= parseFloat(velocitySlider.value)) {
         frequencySlider.value = velocitySlider.value;
+        // frequencyValueDisplay.textContent = velocity.toFixed(1);
+        // wavelengthValueDisplay.textContent = wavelength.toFixed(1);
         wavelengthSlider.value = wavelengthSlider.min;
+    } else {
+        wavelengthSlider.value = (velocitySlider.value / frequencySlider.value).toFixed(1);
+        // wavelengthValueDisplay.textContent = (velocitySlider.value / frequencySlider.value).toFixed(1);
     }
 });
 
 wavelengthSlider.addEventListener('input', function () {
     if (parseFloat(velocitySlider.value) <= parseFloat(wavelengthSlider.value)) {
         velocitySlider.value = wavelengthSlider.value;
+        // velocityValueDisplay.textContent = wavelength.toFixed(1);
+        // frequencyValueDisplay.textContent = frequency.toFixed(1);
         frequencySlider.value = frequencySlider.min;
     } else if (parseFloat(wavelengthSlider.value) >= parseFloat(velocitySlider.value)) {
         wavelengthSlider.value = velocitySlider.value;
+        // wavelengthValueDisplay.textContent = velocity.toFixed(1);
+        // frequencyValueDisplay.textContent = frequency.toFixed(1);
         frequencySlider.value = frequencySlider.min;
     } else {
         frequencySlider.value = (velocitySlider.value / wavelengthSlider.value).toFixed(1);
+        // frequencyValueDisplay.textContent = (velocitySlider.value / wavelengthSlider.value).toFixed(1);
     }
 });
 
 velocitySlider.addEventListener('input', function () {
     if (parseFloat(wavelengthSlider.value) == parseFloat(wavelengthSlider.min) && parseFloat(velocitySlider.value) < parseFloat(frequencySlider.value)) {
         frequencySlider.value = velocitySlider.value;
+        // frequencyValueDisplay.textContent = velocity.toFixed(1);
         wavelengthSlider.value = wavelengthSlider.min;
+        // wavelengthValueDisplay.textContent = wavelength.toFixed(1);
     }
 });
 
-// const minValue = velocitySlider.min;
-// const maxValue = velocitySlider.max;
+const minValue = velocitySlider.min;
+const maxValue = velocitySlider.max;
+// console.log(minValue);
+// console.log(maxValue);
 
 // // // // // // // // // // // // // // // // // // // 
 
-const canvas = document.getElementById("wave");
+const canvas = document.getElementById("amplitude-wave");
 const ctx = canvas.getContext("2d");
 const width = canvas.width;
 const height = canvas.height;
@@ -71,6 +107,8 @@ let amplitude = 50;
 let frequency = 1;
 let wavelength = 160;
 let phase = 0;
+// let targetRotationY = 0;
+// let targetRotationX = 0;
 let rotationY = 0;
 let rotationX = 0;
 
@@ -94,7 +132,7 @@ function drawSineWave() {
         ctx.lineTo(projectedX, projectedY);
     }
 
-    ctx.strokeStyle = "white";
+    ctx.strokeStyle = "black";
     ctx.lineWidth = 4;
     ctx.stroke();
     ctx.restore();
@@ -102,6 +140,9 @@ function drawSineWave() {
 
 function animate() {
     phase += velocity / wavelength / 2;
+    // rotationX += (targetRotationX - rotationX) * 0.1;
+    // rotationY += (targetRotationY - rotationY) * 0.1;
+
     drawSineWave();
     requestAnimationFrame(animate);
 }
@@ -109,27 +150,43 @@ function animate() {
 animate();
 log();
 
-const amplitudeSlider = document.getElementById('amplitude');
+const soloAmplitudeSlider = document.getElementById('solo-amplitude');
+// const amplitudeValueDisplay = document.getElementById('amplitudeValue');
+
 const minOpacity = 0.2;
 const maxOpacity = 2;
 
-amplitudeSlider.addEventListener('input', () => {
-    amplitude = parseFloat(amplitudeSlider.value);
+soloAmplitudeSlider.addEventListener('input', () => {
+    amplitude = parseFloat(soloAmplitudeSlider.value);
+    // amplitudeValueDisplay.textContent = Math.round(amplitude);
     log();
     const opacity = minOpacity + (amplitude / 100) * (maxOpacity - minOpacity);
     colorRect.style.opacity = opacity;
     console.log(opacity)
+    // colorRect.style.width = '400px';
+    // colorRect.style.height = '400px';
 
-    if (amplitude >= 51 && amplitude <= 100) {
-        const minHeight = 600;
-        const maxHeight = 1200;
-        const minWidth = 800;
-        const maxWidth = 1600;
-        const width = minWidth + ((amplitude - 51) / 49) * (maxWidth - minWidth);
-        const height = minHeight + ((amplitude - 51) / 49) * (maxHeight - minHeight);
-        colorRect.style.height = `${height}px`;
-        colorRect.style.width = `${width}px`;
-    }
+    // if (amplitude >= 51 && amplitude <= 100) {
+    //     const minHeight = 100;
+    //     const maxHeight = 150;
+    //     const minWidth = 100;
+    //     const maxWidth = 200;
+    //     const height = minHeight + ((amplitude - 51) / 49) * (maxHeight - minHeight);
+    //     const width = minWidth + ((amplitude - 51) / 49) * (maxWidth - minWidth);
+    //     colorRect.style.height = `${height}vh`;
+    //     colorRect.style.width = `${width}vw`;
+    // }
+
+    // if (amplitude >= 51 && amplitude <= 100) {
+    //     const minHeight = 600;
+    //     const maxHeight = 1200;
+    //     const minWidth = 800;
+    //     const maxWidth = 1600;
+    //     const width = minWidth + ((amplitude - 51) / 49) * (maxWidth - minWidth);
+    //     const height = minHeight + ((amplitude - 51) / 49) * (maxHeight - minHeight);
+    //     colorRect.style.height = `${height}px`;
+    //     colorRect.style.width = `${width}px`;
+    // }
 });
 
 frequencySlider.addEventListener('input', () => {
@@ -147,19 +204,19 @@ velocitySlider.addEventListener('input', () => {
 function updateFrequency() {
     const frequencyValue = parseFloat(frequencySlider.value);
     frequency = (velocity / frequencyValue);
-    log();
+    // log();
 }
 
 function updateWavelength() {
     const wavelengthValue = parseFloat(wavelengthSlider.value);
     wavelength = wavelengthValue * 100;
-    log();
+    // log();
 }
 
 function updateVelocity() {
     const velocityValue = parseFloat(velocitySlider.value);
     velocity = velocityValue;
-    log();
+    // log();
 }
 
 function updateWaveValues() {
@@ -170,11 +227,12 @@ function updateWaveValues() {
 }
 
 function log() {
-    console.clear();
-    console.log('frequency', frequency);
-    console.log('wavelength', wavelength);
-    console.log('velocity', velocity);
-    console.log('phase', phase);
+    // console.clear();
+    // console.log('frequency', frequency);
+    // console.log('wavelength', wavelength);
+    // console.log('velocity', velocity);
+    // console.log('phase', phase);
+    // console.log(opacity)
 }
 
 function wavelengthToColor(wavelength) {
@@ -218,3 +276,4 @@ function wavelengthToColor(wavelength) {
     const color = `rgba(${r},${g},${b},${alpha})`;
     document.documentElement.style.setProperty('--input-color', color);
 }
+
